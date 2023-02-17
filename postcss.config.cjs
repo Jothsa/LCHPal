daa1433bcc6e9e3ebd33d45d87c877272a3eb3dc
Scooby08 <58094796+Scooby08@users.noreply.github.com>
2023-02-17 14:31:57 -0600

    formatting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,15 +3,15 @@ const presetEnv = require('postcss-preset-env');
 const openProps = require('open-props');
 
 const config = {
-    plugins: [
-        jit(openProps),
-        presetEnv({
-            stage: 2,
-            features: {
-                'nesting-rules': true,
-            },
-        }),
-    ],
+  plugins: [
+    jit(openProps),
+    presetEnv({
+      stage: 2,
+      features: {
+        'nesting-rules': true,
+      },
+    }),
+  ],
 };
 
 module.exports = config;
