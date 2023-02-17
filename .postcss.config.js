c175377c30f04d80e26be3ba8515838d6d25ad92
Scooby08 <58094796+Scooby08@users.noreply.github.com>
2023-02-17 12:49:11 -0600

    post css

--- .postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: {
-        autoprefixer: {},
-        "postcss-jit-props": {
-            files: ["node_modules/open-props/open-props.min.js"],
-        },
-        "preset-env": {},
-    },
-};
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,17 @@
-const autoprefixer = require("autoprefixer");
+const jit = require('postcss-jit-props');
+const presetEnv = require('postcss-preset-env');
+const openProps = require('open-props');
 
 const config = {
-  plugins: [autoprefixer]
+    plugins: [
+        jit(openProps),
+        postcssPresetEnv({
+            stage: 3,
+            features: {
+                'nesting-rules': true,
+            },
+        }),
+    ],
 };
 
 module.exports = config;
